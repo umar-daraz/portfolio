423c743d88f359dbcd355d11000f22a520ee5d22
umar.daraz <umar.daraz@seventechnology.co.uk>
2018-11-14 18:37:07 +0500

    Add Links

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 import About from '../components/About';
+//import { library } from '@fortawesome/fontawesome-svg-core'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import {faLinkedin} from '@fortawesome/free-brands-svg-icons'
+//console.log(icons)
+//library.add(faLinkedin)
+
 const App = () => (
   <div>
     <About />
